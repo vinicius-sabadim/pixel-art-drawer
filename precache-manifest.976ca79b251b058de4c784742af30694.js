f721d05244f4aed88b49aa00c8669577cb82ee96
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-07-19 11:31:50 +0200

    Updates

---
+++ precache-manifest.976ca79b251b058de4c784742af30694.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a7828edc5399da5ef8973990687484",
+    "url": "/pixel-art-drawer/index.html"
+  },
+  {
+    "revision": "4c8b90b268079b825f0b",
+    "url": "/pixel-art-drawer/static/css/main.37edf6c9.chunk.css"
+  },
+  {
+    "revision": "55c792ef69cffcad6209",
+    "url": "/pixel-art-drawer/static/js/2.27da7bf1.chunk.js"
+  },
+  {
+    "revision": "4c8b90b268079b825f0b",
+    "url": "/pixel-art-drawer/static/js/main.04cde4e6.chunk.js"
+  },
+  {
+    "revision": "35aff08199546bcacc9c",
+    "url": "/pixel-art-drawer/static/js/runtime~main.5b7ff2f2.js"
+  },
+  {
+    "revision": "fe184838e18886d19072788411e0185f",
+    "url": "/pixel-art-drawer/static/media/brush.fe184838.png"
+  },
+  {
+    "revision": "1bcd3a4be772afa450e08da592e9382f",
+    "url": "/pixel-art-drawer/static/media/eraser.1bcd3a4b.png"
+  },
+  {
+    "revision": "f4e77384480d3645dc180aa5db48cf7d",
+    "url": "/pixel-art-drawer/static/media/pencil.f4e77384.png"
+  }
+]);
